58702115964c3b8d4a215bd755ce8ad8f2d93b04
Konrad Daleske <konrad@daleske.de>
2019-10-06 11:26:47 +0200

    Adding gitattributes
    For correct classifying of programming language in github

--- Private/Get-FileLocationConfigurationsCsv.ps1
+++
@@ -1,8 +0,0 @@
-﻿function Get-FileLocationConfigurationsCsv {
-
-    $subPath = "/Configurations/configurations.csv"
-    $moduleFolder = Split-Path -Path $PSScriptRoot -Parent
-    $filePath = Join-Path -Path $moduleFolder -ChildPath $subPath
-
-    return $filePath
-}
